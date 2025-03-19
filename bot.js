28c2c22a2f4085d2911900e6d8ecb2edcec40b69
Moviebotapi <Moviebot317@gmail.com>
2025-03-19 17:27:09 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
 var TelegramBot= require('node-telegram-bot-api')
+const axios = require('axios');
+const cheerio = require('cheerio');
+	
 
 token="YOUR_TELEGRAM_BOT_TOKEN"  //token obtained from bot father
 var bot= new TelegramBot(token, {polling:true});
@@ -9,8 +12,21 @@ var reques=require('request')
 bot.onText(/\/movie (.+)/,function(msg,match){
     var movie= match[1];
     var chatId=msg.chat.id
-    reques('http://www.omdbapi.com/?t='+movie+'&apikey=YOUR_OMDB_KEY',function(error,response,body){
-		
+ //   reques('http://www.omdbapi.com/?t='+movie+'&apikey=YOUR_OMDB_KEY',function(error,response,body){
+const url = `${movie}`;
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+
+      const title = $("div.banners-right > h1").text().trim();
+      const imdb = $("div.jws-imdb").text().trim();
+      const desc = $("div.description").text().trim(); // Changed selector to something more likely for description
+      const year = $("span.video-years").text().trim();
+      const duration = $("span.video-time").text().trim();
+      const type1 = $("div.jws-category > a:nth-child(1)").text().trim();
+      const type2 = $("div.jws-category > a:nth-child(2)").text().trim();
+      const type3 = $("div.jws-category > a:nth-child(3)").text().trim();
+      const image = $("div.jws-images > img").attr('src');
+	
 		//key obtained from omdbapi
         
 		if(!error && response.statusCode==200)
@@ -19,9 +35,9 @@ bot.onText(/\/movie (.+)/,function(msg,match){
             .then(msg)
             {
                 res=JSON.parse(body)
-                bot.sendPhoto(chatId, res.Poster, {caption: 'Result:\nTitle: '+ res.Title +'\nGenre: '+ res.Genre +'\nRated: '+ res.Rated+ '\nReleased: '+ res.Released})
+                bot.sendPhoto(chatId, image, {caption: 'Result:\nTitle: '+ title +'\nGenre: '+ type +'\nRated: '+ imdb+ '\nReleased: '+ year})
             }
         }
-    })
+ //   })
 
 })
